5b580837f5d017f40c718ef636527a681b8d8f17
Jack TP <jacktroncos20@gmail.com>
2024-07-16 19:51:11 -0500

    local_clone

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'subtitle-color':'#696969',
+				'title-color': '#171717',
+				'input-color': '#E3E3E3',
+				'input-placeholder' : '#585858',
+				'hover-button':'#383838',
+				'border-button':'#E0E0E0',
+				'menu-text': '#666',
+			},
+			fontSize: {
+				'3xl':'32px'
+			}
+		},
+	},
+	plugins: [],
+}
